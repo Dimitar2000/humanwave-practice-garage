bbfa132ec95a30384f3fc98e08f0095f77ad3fcb
John Doe <johndoe@example.com>
2023-10-31 11:50:07 +0100

    Add simple CarListItem component.

---
+++ web/src/js/components/car/car-list-item.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Name: {{ name }}</h2>
+    <h2>Owner: {{ owner }}</h2>
+    <h3>Price: {{ price }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            required: true
+        },
+        owner: {
+            required: true
+        },
+        price: {
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

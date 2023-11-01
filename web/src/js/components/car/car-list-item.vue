560ce7a570bfa8c9dbe0afc74960d4a6fdc468ca
John Doe <johndoe@example.com>
2023-11-01 09:07:58 +0100

    Add edit form to car

--- web/src/js/components/car/car-list-item.vue
+++ web/src/js/components/car/car-list-item.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="card">
-  <ul class="list-group">
-    <li class="list-group-item">
-        Name: <label>{{ name }}</label>
-    </li>
-    <li class="list-group-item">
-        Owner: <label>{{ owner }}</label>
-    </li>
-    <li class="list-group-item">
-        Price: <label>{{ price }}</label>
-    </li>
-  </ul>
+    <button @click="toggleEditForm">Edit</button>
+    <p class="car-data">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Name: <label>{{ name }}</label>
+            </li>
+            <li class="list-group-item">
+                Owner: <label>{{ owner }}</label>
+            </li>
+            <li class="list-group-item">
+                Price: <label>{{ price }}</label>
+            </li>
+        </ul>
+        <car-form v-if="editing"></car-form>
+    </p>
   </div>
 </template>
 
 <script>
+import carForm from './car-form.vue'
 export default {
+    components: { carForm },
+    emits: ['edit-car'],
     props: {
         name: {
             required: true
@@ -27,7 +34,33 @@ export default {
             required: true,
             type: Number
         }
-    }
+    },
+    
+    data() {
+        return {
+            editing: false,
+            updatedCar: {
+                name: null,
+                owner: null,
+                price: null
+            }
+        }
+    },
+
+    created() {
+        // Copy props to updated car
+        this.updatedCar = {
+            name: this.name,
+            owner: this.owner,
+            price: this.price
+        }
+    },
+    
+    methods: {
+        toggleEditForm() {
+            this.editing = !this.editing
+        }
+    },
 }
 </script>
 

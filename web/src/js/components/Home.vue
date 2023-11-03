96f26e6a49fb6857495d9021393fa4b4c6468e48
John Doe <johndoe@example.com>
2023-11-03 10:14:07 +0100

    Fix: Use fetch instead of ajax - same origin policy problem

--- web/src/js/components/Home.vue
+++ web/src/js/components/Home.vue
@@ -18,13 +18,24 @@
         
         methods: {
             redirectRequest() {
-                $.ajax({
-                    type: 'GET',
-                    contentType: 'application/json',
-                    url: `/garages/worker`
-                }).then((data) => {
-                    console.log(data);
-                })
+                fetch('/garages/worker')
+                .then(res => res.json())
+                .then(res => console.log(res));
+
+                // $.ajax({
+                //     type: 'GET',
+                //     contentType: 'application/json',
+                //     url: `/garages/worker`,
+                //     success: function (data, textStatus) {
+                //         if (data.redirect) {
+                //         // data.redirect contains the string URL to redirect to
+                //         window.location.replace(data.redirect);
+                //         } else {
+                //         // data.form contains the HTML for the replacement form
+                //             console.log("No redirect");
+                //         }
+                //     }
+                // });
             }
         }
     }

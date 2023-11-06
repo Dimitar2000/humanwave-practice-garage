3ce16fe162ecb3a4162989aa923c2650ec80ce4a
John Doe <johndoe@example.com>
2023-11-06 09:49:49 +0100

    Worker: Fix unequal allowed origin.

--- worker/main.py
+++ worker/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello():
     response = jsonify(["Worker"])
-    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
+    response.headers.add('Access-Control-Allow-Origin', 'http://127.0.0.1:8080')
     return response
 
 if __name__ == "__main__":
